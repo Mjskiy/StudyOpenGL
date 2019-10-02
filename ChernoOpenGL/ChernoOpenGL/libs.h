09f5c00e2cd1891ceb9427e22c7bc2dc0bd06a46
Mjskiy <kiy9025@gmail.com>
2019-10-02 16:31:14 +0900

    add local project test

---
+++ ChernoOpenGL/ChernoOpenGL/libs.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+#include <glew.h>
+#include <glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+
+//#include <glm.hpp>
+//#include <vec2.hpp>
+//#include <vec3.hpp>
+//#include <vec4.hpp>
+//#include <mat4x2.hpp>
+//#include <mat4x3.hpp>
+//#include <mat4x4.hpp>
+//#include <gtc\matrix_transform.hpp>
+//#include <gtc\type_ptr.hpp>
